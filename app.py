aaa093e295aa0f0cd4c60e647c02be84c7f0c6f5
Galuh Apriliano <galuhapriliano30@gmail.com>
2024-02-17 23:05:37 +0700

    fix function

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
-# Import Library
 import komputasi
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import streamlit as st
 from PIL import Image
-from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import association_rules, apriori
 
-col1, col2 = st.columns(2)
-col1.title('Data Mining Apriori')
-col1.write('OPTIMASI STOK BARANG DENGAN ANALISIS PENJUALAN MENGGUNAKAN METODE ASSOCIATION RULE MINING DI TOKO HANDARI')
+# Menampilkan judul dan deskripsi
+st.title('Data Mining Apriori')
+st.write('OPTIMASI STOK BARANG DENGAN ANALISIS PENJUALAN MENGGUNAKAN METODE ASSOCIATION RULE MINING DI TOKO HANDARI')
 
-
+# Menampilkan gambar
 image = Image.open('images/image.jpg')
 st.image(image)
+
+# Memuat dataset
 df = None
 dataset_file = st.file_uploader("Upload Dataset Anda", type=['csv'])
-try:
-    df = pd.read_csv(dataset_file)
-except:
+
+# Menangani kesalahan saat memuat dataset
+if dataset_file is None:
     st.warning('Mohon upload dataset Anda!')
     st.stop()
 
-# Get Cols Names
-pembeli = df.columns[0]
-tanggal = df.columns[1]
-produk = df.columns[2]
+try:
+    df = pd.read_csv(dataset_file)
+except Exception as e:
+    st.error(f"Terjadi kesalahan saat membaca file: {str(e)}")
+    st.stop()
 
-# Data Mining
-df = komputasi.data_summary(df, pembeli, tanggal, produk)
+# Mendapatkan nama kolom
+if df is not None and not df.empty:
+    pembeli, tanggal, produk = df.columns[0], df.columns[1], df.columns[2]
 
-# MBA using Apriori
-komputasi.MBA(df, pembeli, produk)
+    # Memanggil fungsi untuk prapemrosesan data
+    df = komputasi.data_summary(df, pembeli, tanggal, produk)
+
+    # Memanggil fungsi untuk melakukan Association Rule Mining menggunakan Apriori
+    komputasi.MBA(df, pembeli, produk)
+else:
+    st.warning("Dataset kosong atau tidak valid. Mohon unggah dataset yang valid.")
