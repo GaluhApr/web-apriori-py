0d4fdf2f08f62c5cf903a607290ef4b414fc392f
Galuh Apriliano <galuhapriliano30@gmail.com>
2024-02-17 21:27:08 +0700

    fix error chart

--- komputasi.py
+++ komputasi.py
@@ -49,9 +49,7 @@ def show_transaction_info(df, produk, pembeli):
         most_sold = df[produk].value_counts().tail(jumlah)
         most_sold = most_sold.sort_values(ascending=True)
     c1, c2 = st.columns((2, 1))
-    most_sold.plot(kind='bar')
-    plt.title('Jumlah Produk Terjual')
-    c1.pyplot(plt)
+    c1.bar_chart(most_sold)  # Menggunakan method bar_chart untuk menampilkan plot
     c2.write(most_sold)
 
 def data_summary(df, pembeli, tanggal, produk):
