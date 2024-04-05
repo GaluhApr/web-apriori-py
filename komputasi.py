77b942b092be72dd40cb9c7e7e197e282b4d76b7
Galuh Apriliano <galuhapriliano30@gmail.com>
2024-04-06 01:58:57 +0700

    update mba

--- komputasi.py
+++ komputasi.py
@@ -164,7 +164,7 @@ def MBA(df, pembeli, produk):
             most_sold = df[produk].value_counts()
             if not most_sold.empty:
                 col2.subheader("Jumlah Produk Terjual")
-                col2.write(most_sold)
+                col2.write(most_sold,width=800)
             else:
                 st.warning("Tidak ada data yang sesuai dengan kriteria yang dipilih.")
             
