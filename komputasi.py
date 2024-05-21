0579bc517723bb5409a3430e8422ed6469a86e48
Galuh304 <galuhapriliano30@gmail.com>
2024-05-21 15:41:20 +0700

    update support

--- komputasi.py
+++ komputasi.py
@@ -5,20 +5,9 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
-import time
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import association_rules, apriori
 from sklearn.preprocessing import MinMaxScaler
-
-def normalize_data(df):
-    scaler = MinMaxScaler()
-    df[['Tanggal', 'Bulan', 'Tahun']] = scaler.fit_transform(df[['Tanggal', 'Bulan', 'Tahun']])
-    return df
-
-def preprocess_data(df, tanggal, sep, dateformat):
-    df = prep_date(df, tanggal, sep, dateformat)
-    df = normalize_data(df)
-    return df
 
 def prep_date(df, tanggal, sep, dateformat):
     if dateformat == 'ddmmyy':
@@ -33,6 +22,16 @@ def prep_date(df, tanggal, sep, dateformat):
         df['Tanggal'] = df[tanggal].apply(lambda x: int(x.split(sep)[2]))
         df['Bulan'] = df[tanggal].apply(lambda x: int(x.split(sep)[1]))
         df['Tahun'] = df[tanggal].apply(lambda x: int(x.split(sep)[0]))
+    return df
+
+def normalize_data(df):
+    scaler = MinMaxScaler()
+    df[['Tanggal', 'Bulan', 'Tahun']] = scaler.fit_transform(df[['Tanggal', 'Bulan', 'Tahun']])
+    return df
+
+def preprocess_data(df, tanggal, sep, dateformat):
+    df = prep_date(df, tanggal, sep, dateformat)
+    df = normalize_data(df)
     return df
 
 def dataset_settings(df, pembeli, tanggal, produk):
@@ -52,11 +51,8 @@ def show_transaction_info(df, produk, pembeli):
         st.subheader(f'Informasi Transaksi:')
         total_produk = df[produk].nunique()
         total_transaksi = df[pembeli].nunique()
-        total_barang_terjual = df[produk].sum()  # Menghitung jumlah total barang terjual
-        total_frekuensi_produk = len(df)  # Menghitung frekuensi total dari semua produk
-        col1.info(f'Produk terjual     : {total_produk}')
+        col1.info(f'Total produk     : {total_produk}')
         col2.info(f'Total transaksi  : {total_transaksi}')
-        col2.info(f'Frekuensi total produk terjual  : {total_frekuensi_produk}')  # Menampilkan frekuensi total produk terjual
         sort = col1.radio('Tentukan kategori produk', ('Terlaris', 'Kurang Laris'))
         jumlah = col2.slider('Tentukan jumlah produk', 0, total_produk, 10)
         if sort == 'Terlaris':
@@ -66,16 +62,14 @@ def show_transaction_info(df, produk, pembeli):
             most_sold = most_sold.sort_values(ascending=True)
         if not most_sold.empty:
             c1, c2 = st.columns((2, 1))
-            plt.figure(figsize=(8, 4)) 
             most_sold.plot(kind='bar')
-            plt.title('Grafik Penjualan')
+            plt.title('Jumlah Produk Terjual')
             c1.pyplot(plt)
             c2.write(most_sold)
         else:
             st.warning("Tidak ada data yang sesuai dengan kriteria yang dipilih.")
     except Exception as e:
         st.error(f"Terjadi kesalahan saat menampilkan informasi transaksi: {str(e)}")
-
 
 def data_summary(df, pembeli, tanggal, produk):
     st.header('Ringkasan Dataset')
@@ -105,8 +99,6 @@ def prep_frozenset(rules):
 def MBA(df, pembeli, produk):
     st.header('Association Rule Mining Menggunakan Apriori')
     if st.button("Mulai Perhitungan Asosiasi"):
-        start_time = time.time()  
-
         transaction_list = []
         for i in df[pembeli].unique():
             tlist = list(set(df[df[pembeli]==i][produk]))
@@ -115,22 +107,15 @@ def MBA(df, pembeli, produk):
         te = TransactionEncoder()
         te_ary = te.fit(transaction_list).transform(transaction_list)
         df2 = pd.DataFrame(te_ary, columns=te.columns_)
-        frequent_itemsets = apriori(df2, min_support=0.005, use_colnames=True)   #nilai support yang digunakan
+        frequent_itemsets = apriori(df2, min_support=0.02, use_colnames=True)   #nilai support yang digunakan
         try:
-            rules = association_rules(frequent_itemsets, metric='confidence', min_threshold=0.1) 
-            # Ganti min_threshold sesuai dengan nilai confidence yang diinginkan
+            rules = association_rules(frequent_itemsets, metric='lift', min_threshold=0.01) #nilai confidence yang digunakan
         except ValueError as e:
             st.error(f"Terjadi kesalahan saat menghasilkan aturan asosiasi: {str(e)}")
             st.stop()
 
-        end_time = time.time()  
-        processing_time = end_time - start_time  
-
-        col1, col2 = st.columns(2)
-        col1.subheader('Hasil Rules (Pola Pembelian Pelanggan)')
+        st.subheader('Hasil Rules')
         st.write('Total rules yang dihasilkan :', len(rules))
-        col1.write(f'Waktu yang dibutuhkan untuk memproses rule: {processing_time:.2f} detik')
-
         if len(rules) == 0:  # Tidak ada aturan yang dihasilkan
             st.write("Tidak ada aturan yang dihasilkan.")
         else:
@@ -141,49 +126,50 @@ def MBA(df, pembeli, produk):
                 'consequents': consequents,
                 'support': rules['support'],
                 'confidence': rules['confidence'],
-                'lift ratio': rules['lift'],
+                'lift': rules['lift'],
                 'contribution': rules['support'] * rules['confidence']
             }
             matrix = pd.DataFrame(matrix)
             matrix.reset_index(drop=True, inplace=True)
-            matrix.index += 1
-            col1.write(matrix)
-            col2.subheader('Keterangan')
-            col2.write("- Support = Seberapa sering sebuah rules tersebut muncul dalam data,")
-            col2.write("- Confidence = Seberapa sering rules tersebut dikatakan benar")
-            col2.write("- Lift Ratio = Ukuran Kekuatan hubungan antara dua item")
-            col2.write("- Contribution = Kontribusi setiap rules terhadap peningkatan lift secara keseluruhan")
+            matrix.index += 1 
+            st.write(matrix) # Menampilkan seluruh hasil rule
             
-            # Tampilkan rekomendasi stok barang untuk dibeli
-            col1, col2 = st.columns(2)
-            col1.subheader("Rekomendasi stok barang untuk dibeli (contribution) :")
+            st.write('Support')
+            st.write('- Support mengindikasikan seberapa sering itemset tertentu muncul dalam dataset transaksi')
+            st.write('- Semakin tinggi nilai support, semakin sering itemset tersebut muncul dalam transaksi, yang menunjukkan bahwa itemset tersebut relatif populer atau sering dibeli bersama')
+            st.write('Confidence')
+            st.write('- confidence mengindikasikan seberapa sering itemset A dan itemset B muncul bersamaan dalam transaksi, dibandingkan dengan seberapa sering itemset A muncul sendiri')
+            st.write('- Nilai confidence yang tinggi menunjukkan bahwa aturan asosiasi tersebut memiliki kecenderungan yang kuat untuk terjadi')
+            st.write('Lift')
+            st.write('- Lift merupakan ukuran kekuatan aturan asosiasi')
+            st.write('- Nilai lift lebih dari 1 menunjukkan bahwa itemset A dan itemset B muncul bersamaan lebih sering dari yang diharapkan secara acak, yang menunjukkan adanya korelasi positif antara keduanya')
+            st.write('- Lift 1 menunjukkan bahwa tidak ada korelasi antara itemset A dan itemset B. Lift lebih kecil dari 1 menunjukkan adanya korelasi negatif antara keduanya')
+            st.write('Contribution')
+            st.write('- Kontribusi aturan menunjukkan seberapa besar aturan tersebut berkontribusi terhadap rekomendasi stok barang')
+            st.write('- Semakin tinggi kontribusi semakin penting aturan tersebut dalam pembentukan rekomendasi.')
+        
+            # Menambahkan rekomendasi stok barang untuk dibeli berdasarkan kontribusi
             recommended_products = []
             recommended_products_contribution = {}
-            for antecedent, contribution in zip(matrix['antecedents'], matrix['contribution']):
-                antecedent_list = antecedent.split(', ')
-                for item in antecedent_list:
+            for consequent, contribution in zip(matrix['consequents'], matrix['contribution']):
+                consequent_list = consequent.split(', ')
+                for item in consequent_list:
                     if item not in recommended_products_contribution:
                         recommended_products_contribution[item] = contribution
                     else:
                         recommended_products_contribution[item] += contribution
-                recommended_products.extend(antecedent_list)
+                recommended_products.extend(consequent_list)
             recommended_products = list(set(recommended_products))  # Hapus duplikat
-            recommended_products_sorted = sorted(recommended_products, key=lambda x: (recommended_products_contribution[x], matrix[matrix['antecedents'].apply(lambda y: x in y)]['lift ratio'].values[0]), reverse=True)
-            for idx, item in enumerate(recommended_products_sorted, start=1):
-                col1.write(f"{idx}. <font color='red'>{item}</font> ({recommended_products_contribution[item]})", unsafe_allow_html=True)
 
-            # Tampilkan informasi tentang produk yang paling laris terjual dalam bentuk tabel
-            most_sold = df[produk].value_counts()
-            if not most_sold.empty:
-                col2.subheader("Jumlah Produk Terjual")
-                col2.dataframe(most_sold, width=400)  
-            else:
-                st.warning("Tidak ada data yang sesuai dengan kriteria yang dipilih.")
-            
-            for a, c, supp, conf, lift in sorted(zip(matrix['antecedents'], matrix['consequents'], matrix['support'], matrix['confidence'], matrix['lift ratio']), key=lambda x: x[4], reverse=True):
-                st.info(f'Jika customer membeli {a}, maka customer juga membeli {c}')
+            st.subheader("Rekomendasi stok barang untuk dibeli (contribution) :")
+            recommended_products_sorted = sorted(recommended_products, key=lambda x: (recommended_products_contribution[x], matrix[matrix['consequents'].apply(lambda y: x in y)]['lift'].values[0]), reverse=True)
+            for idx, item in enumerate(recommended_products_sorted, start=1):
+                st.write(f"{idx}. <font color='red'>{item}</font> ({recommended_products_contribution[item]})", unsafe_allow_html=True)
+
+            for a, c, supp, conf, lift in sorted(zip(matrix['antecedents'], matrix['consequents'], matrix['support'], matrix['confidence'], matrix['lift']), key=lambda x: x[4], reverse=True):
+                st.info(f'Jika customer membeli {a}, maka ia membeli {c}')
                 st.write('Support : {:.3f}'.format(supp))
                 st.write('Confidence : {:.3f}'.format(conf))
-                st.write('Lift Ratio : {:.3f}'.format(lift))
+                st.write('Lift : {:.3f}'.format(lift))
                 st.write('Contribution : {:.3f}'.format(supp * conf))
                 st.write('')
